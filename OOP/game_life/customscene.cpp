ac3aa5084f9ae0a9a69867f8c373e4dbc2c30add
levkoLuk <l.lukyanets@g.nsu.ru>
2025-01-21 18:26:28 +0700

    mouseMoveEvent

--- OOP/game_life/customscene.cpp
+++ OOP/game_life/customscene.cpp
@@ -1,12 +1,28 @@
 #include "customscene.h"
 
-CustomScene::CustomScene(QObject *parent) : QGraphicsScene(parent) {
-
+CustomScene::CustomScene(QObject *parent) : QGraphicsScene(parent)
+{
 }
 
-void CustomScene::mousePressEvent(QGraphicsSceneMouseEvent *event) {
+void CustomScene::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
     QPointF scenePos = event->scenePos();
     int x = static_cast<int>(scenePos.x()) / 10;
     int y = static_cast<int>(scenePos.y()) / 10;
     emit cellClicked(x, y);
+}
+
+void CustomScene::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton) {
+        QPointF scenePos = event->scenePos();
+        int x = static_cast<int>(scenePos.x()) / 10;
+        int y = static_cast<int>(scenePos.y()) / 10;
+
+        if (x != lastX || y != lastY) {
+            lastX = x;
+            lastY = y;
+            emit cellClicked(x, y);
+        }
+    }
 }
