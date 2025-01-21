54a8d2017c885aa86a98bac6dec95766e111eeb6
levkoLuk <l.lukyanets@g.nsu.ru>
2025-01-21 18:29:08 +0700

    Update gameoflife.h  enum CellState

--- OOP/game_life/gameoflife.h
+++ OOP/game_life/gameoflife.h
@@ -1,22 +1,29 @@
-#ifndef GAMEOFLIFE_H
-#define GAMEOFLIFE_H
+#pragma once
 
 #include <vector>
 
-class GameOfLife {
+class GameOfLife
+{
 public:
+    enum CellState {
+        Dead,
+        Alive,
+        Zombie
+    };
+
     GameOfLife(int width, int height);
-    bool getCellState(int x, int y) const;
+    CellState getCellState(int x, int y) const;
     void toggleCellState(int x, int y);
     void update();
     void reset();
+    void setZombieMode(bool enabled);
+    int getZombieCount() const;
 
 private:
     int width;
     int height;
-    std::vector<std::vector<bool>> grid;
+    std::vector<std::vector<CellState>> grid;
+    bool zombieMode;
 
     int countLiveNeighbors(int x, int y) const;
 };
-
-#endif // GAMEOFLIFE_H
