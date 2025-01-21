6b38f6ee482b1dc63cc11342495b10c541848243
levkoLuk <l.lukyanets@g.nsu.ru>
2025-01-21 18:30:58 +0700

    Update mainwindow.cpp

--- OOP/game_life/mainwindow.cpp
+++ OOP/game_life/mainwindow.cpp
@@ -5,7 +5,7 @@
 #include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow), scene(new CustomScene(this)), game(new GameOfLife(50, 50)), timer(new QTimer(this))
+    : QMainWindow(parent), ui(new Ui::MainWindow), scene(new CustomScene(this)), game(new GameOfLife(50, 50)), timer(new QTimer(this)), zombieMode(false)
 {
     ui->setupUi(this);
     ui->graphicsView->setScene(scene);
@@ -15,21 +15,23 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->startButton, &QPushButton::clicked, this, &MainWindow::onStartButtonClicked);
     connect(ui->stopButton, &QPushButton::clicked, this, &MainWindow::onStopButtonClicked);
     connect(ui->resetButton, &QPushButton::clicked, this, &MainWindow::onResetButtonClicked);
+    connect(ui->zombieModeButton, &QPushButton::clicked, this, &MainWindow::onZombieModeButtonClicked);
 
     connect(scene, &CustomScene::cellClicked, this, &MainWindow::onCellClicked);
 
     connect(timer, &QTimer::timeout, this, &MainWindow::updateGame);
+
+    updateZombieCount();
 }
 
-MainWindow::~MainWindow() {
+MainWindow::~MainWindow()
+{
     delete ui;
-    delete scene;
     delete game;
-    delete timer;
 }
 
-void MainWindow::drawGrid() {
-
+void MainWindow::drawGrid()
+{
     scene->clear();
     for (int x = 0; x < 50; ++x) {
         for (int y = 0; y < 50; ++y) {
@@ -37,29 +39,47 @@ void MainWindow::drawGrid() {
             scene->addItem(cell);
         }
     }
+    updateZombieCount();
 }
 
-void MainWindow::onStartButtonClicked() {
-
+void MainWindow::onStartButtonClicked()
+{
     timer->start(100);
 }
 
-void MainWindow::onStopButtonClicked() {
-
+void MainWindow::onStopButtonClicked()
+{
     timer->stop();
 }
 
-void MainWindow::onResetButtonClicked() {
+void MainWindow::onResetButtonClicked()
+{
     game->reset();
     drawGrid();
 }
 
-void MainWindow::updateGame() {
+void MainWindow::onZombieModeButtonClicked()
+{
+    zombieMode = !zombieMode;
+    ui->zombieModeButton->setText(zombieMode ? "Zombie Mode: On" : "Zombie Mode: Off");
+    game->setZombieMode(zombieMode);
+    drawGrid();
+}
+
+void MainWindow::updateGame()
+{
     game->update();
     drawGrid();
 }
 
-void MainWindow::onCellClicked(int x, int y) {
+void MainWindow::onCellClicked(int x, int y)
+{
     game->toggleCellState(x, y);
     drawGrid();
+}
+
+void MainWindow::updateZombieCount()
+{
+    int zombieCount = game->getZombieCount();
+    ui->zombieCountLabel->setText(QString("Zombie Cells: %1").arg(zombieCount));
 }
