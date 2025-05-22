55d32957a81a19d2ee8c2a4314104e6aa2b57ac6
levkoLuk <l.lukyanets@g.nsu.ru>
2025-05-22 07:43:19 +0700

    Update evaluate.py

--- zulu_sentiment_analysis/src/training/evaluate.py
+++ zulu_sentiment_analysis/src/training/evaluate.py
@@ -3,21 +3,17 @@ from sklearn.metrics import classification_report
 import pandas as pd
 
 def evaluate():
-    # Загрузка модели
     classifier = pipeline(
         "text-classification",
         model="models/trained_model",
         tokenizer="models/trained_model"
     )
     
-    # Загрузка тестовых данных
     test_data = pd.read_csv("data/processed/test_dataset.csv")
     
-    # Предсказания
     predictions = classifier(list(test_data['text']))
     predicted_labels = [int(p['label'].split('_')[-1]) for p in predictions]
     
-    # Оценка
     print(classification_report(
         test_data['label'],
         predicted_labels,
